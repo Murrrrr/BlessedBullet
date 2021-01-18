4b3f79c30d59036b408107a1dbb2b9f41d82652a
Murrrrr <41497682+Murrrrr@users.noreply.github.com>
2021-01-19 01:38:57 +0900

    가격비교

---
+++ script/top_item_page_contents.js
@@ -0,0 +1,48 @@
+const main_image_caption = document.querySelector('.main-image_caption');
+const main_image = document.querySelector('.main-image');
+const sort_list = document.querySelectorAll('.item_page_header_sort > span');
+
+var item_page_list_items = document.querySelectorAll('.item_page_list_items');
+var item_price = document.querySelectorAll('.item_price');
+var item_name = document.querySelectorAll('.item_name');
+var item_info_array = [];
+
+function item_info(name, price){
+    this.name = name;
+    this.price = price;
+}
+
+function comparePrice(price1, price2){
+    return price1.price < price2.price ? -1 : price1.price > price2.price ? 1 : 0;
+}
+
+main_image_caption.style.left = `${main_image.clientWidth/2 - main_image_caption.clientWidth/2}px`
+main_image_caption.style.top = `${main_image.clientHeight/2 - main_image_caption.clientHeight/2}px`
+
+for(let i=0; i<sort_list.length; i++){
+    sort_list[i].addEventListener('click',()=>{
+        for(let j=0; j<sort_list.length; j++)
+            sort_list[j].classList.remove('active');
+        sort_list[i].classList.add('active');
+    })
+}
+
+for(let i=0; i<item_page_list_items.length; i++){
+    item_info_array.push(new item_info(item_name[i].innerHTML, item_price[i].innerHTML));
+}
+
+item_info_array.sort(comparePrice);
+for(let i=0; i<item_info_array.length; i++){
+    item_name[i].innerHTML = item_info_array[i].name;
+    item_price[i].innerHTML = item_info_array[i].price;
+}
+
+
+
+
+
+
+// 가격 추출
+// 가격비교
+// 가격 낮은 순으로 배열 값 push
+// 옮긴 배열들을 replace
